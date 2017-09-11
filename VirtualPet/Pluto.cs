8f1b44640c33d6541df0ad2fa7150a87a6804b9d
HollyNLittle <hollyisalwayshopeful@gmail.com>
2017-09-11 15:14:37 -0400

    ready to turn in

---
+++ VirtualPet/Pluto.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Pluto
+    {
+        //fields
+        private int points = 0;
+        private int score = 0;
+        private bool isChoiceCorrect = true;
+        private bool isHungry = true;
+        private bool isBored = true;
+        private bool isSleepy = true;
+
+
+        //properties
+        //public int HairLength { get; set; }
+        //public string Name
+        //    {
+        //    get { return this.name; }
+        //    set { this.name = value; }
+        //    }
+
+        public int Points
+        {
+        get { return points; }
+        set { points = value; }
+        }
+        public int Score
+        {
+            get { return score; }
+            set { score = value; }
+        }
+        public bool IsChoiceCorrect
+        {
+            get { return this.isChoiceCorrect; }
+            set { this.isChoiceCorrect = value; }
+        }
+        public bool IsHungry
+        {
+            get { return this.isHungry; }
+            set { this.isHungry = value; }
+        }
+        public bool IsBored
+        {
+            get { return this.isBored; }
+            set { this.isBored = value; }
+        }
+        public bool IsSleepy
+        {
+            get { return this.isSleepy; }
+            set { this.isSleepy = value; }
+        }
+        //Constructor
+        public Pluto()
+        { }
+
+        //Methods
+        //Could set properties to console.readline in program
+        //then call method and use answer as the param
+        public void Feed(bool isHungry)
+        {
+            this.isHungry = isHungry; //to lower
+            if (isHungry)
+            {
+                Console.WriteLine("Pluto says, Thank you! You are an awesome Pet Parent!!!");
+            }
+            //call Display()
+        }
+        public void Play(bool isBored)
+        {
+            this.isBored = isBored;//add .ToLower
+            if(this.isBored)
+            {
+                Console.WriteLine("Pluto says, Thank you! That was sooo much FUN!!!");
+            }
+        }
+        public void Sleep(bool isSleepy)
+        {
+            this.isSleepy = isSleepy;//add .ToLower
+            if (this.isSleepy)//add .ToLower
+            {
+                Console.WriteLine("Pluto says, Thank you! I am beat!!!");
+            }
+        }
+        public void Display(bool isChoiceCorrect)
+        {
+            this.isChoiceCorrect = isChoiceCorrect;
+            if (isChoiceCorrect)
+            {
+                points++;
+                Console.WriteLine("\n Yay!!!  You get a Pet Parent Point! \n Your score is now: {0}.\n", points);
+            }
+            else
+            {
+                Console.WriteLine("\n Your score is now: {0}.\n", points);
+            }
+        }
+        public void Goodnight(bool isChoiceCorrect, int points)
+        {
+            //Choose message based on answer and wants to or doesn't want to play again.
+            this.isChoiceCorrect = isChoiceCorrect;
+            this.points = points;
+            if (isChoiceCorrect)
+            {
+                Console.WriteLine("\n\n\n                GREAT!!!  Let's go AGAIN!!!!!!\n\n\n");
+            }
+            else
+            {
+                switch (points)
+                {
+                    case 0:
+                        Console.WriteLine("\n\n                 *sigh*\n           That was the WORST, most HORRIBLE, AWFUL day EVER!!!\n");
+                        break;
+                    case 1:
+                        Console.WriteLine("\n\n           Thank you, but your pet parenting skills need a bit of work!\n");
+                        break;
+                    case 2:
+                        Console.WriteLine("\n\n           Thanks, but you are sooo NOT ready to be a pet parent!\n");
+                        break;
+                    case 3:
+                        Console.WriteLine("\n\n           Thank you!!!   \n             I have an AWESOME pet parent!!!!\n");
+                        break;
+                    default:
+                        Console.WriteLine("\n\n           Thank you!!!\n");
+                        break;
+                }
+                Console.WriteLine("\n           See you later, Alligator!\n\n                  Goodnight...zzzzzzzzzzzzzzzzzzzzzz\n\n\n");
+            }
+        }
+
+    }
+}
--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -10,6 +10,34 @@ namespace VirtualPet
     {
         static void Main(string[] args)
         {
+            Pluto usersPluto = new Pluto();
+            usersPluto.IsChoiceCorrect = true;
+
+            do 
+            {
+                Console.WriteLine
+                    ("Good morning, says your favorite pet, Pluto!!!\n   Yaaawwwnnn..............I am sooooo Hungry!!!");
+                Console.WriteLine
+                    ("     If you want to feed Pluto, right now enter True.\n     If you would rather play outside with your friends then enter False.");
+                usersPluto.IsHungry = Convert.ToBoolean(Console.ReadLine().ToLower());
+                usersPluto.Display(usersPluto.IsHungry);
+
+                Console.WriteLine("I am soooo BORED!!!  Can we go to the Pet Park, pleeeeeeeezzzz!!!!!!");
+                Console.WriteLine
+                    ("     If you want to take Pluto to the Pet Park enter True.  \n     If you would rather play video games then enter False.");
+                usersPluto.IsBored = Convert.ToBoolean(Console.ReadLine().ToLower());
+                usersPluto.Display(usersPluto.IsBored);
+
+                Console.WriteLine("I am soooo SLEEPY!!!");
+                Console.WriteLine
+                    ("     If you are going to put Pluto to sleep enter True.  \n     If you would rather watch TV then enter False.");
+                usersPluto.IsSleepy = Convert.ToBoolean(Console.ReadLine().ToLower());
+                usersPluto.Display(usersPluto.IsSleepy);
+
+                Console.WriteLine("Would you like to start another day?  \n If so enter True. \n     If you would rather leave Pluto sleeping then enter False.");
+                usersPluto.IsChoiceCorrect = Convert.ToBoolean(Console.ReadLine().ToLower());
+                usersPluto.Goodnight(usersPluto.IsChoiceCorrect, usersPluto.Points);
+            } while (usersPluto.IsChoiceCorrect == true);
         }
     }
 }
